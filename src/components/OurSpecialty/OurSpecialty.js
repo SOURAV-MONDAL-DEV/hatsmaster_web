a3ed375cd70581f6573f1a3ee8bf62e588cab2b3
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2024-07-12 11:11:45 +0600

    Our specialty section done with dynamic data pass only title and img

--- src/components/OurSpecialty/OurSpecialty.js
+++ src/components/OurSpecialty/OurSpecialty.js
@@ -1,24 +1,32 @@
 // components/OurSpecialty.js
 import React from "react";
 import RotatingBox from "../RotatingBox/RotatingBox";
+import in10 from "@/images/icons/index_10.png";
+import in11 from "@/images/icons/index_11.png";
+import in12 from "@/images/icons/index_12.png";
+import in13 from "@/images/icons/index_13.png";
 
 const specialties = [
   {
     title: "PRICE ADVANTAGE",
     description: "We guarantee we always offer the best price to our clients.",
+    image: in10,
   },
   {
     title: "PRODUCT QUALITY",
     description: "We have our own testing lab and the most advanced and complete inspection equipment, which can ensure the quality of the products.",
+    image: in11,
   },
   {
     title: "EFFICIENT PRODUCTION CAPACITY",
     description: "Our annual production capacity is over 300000 tons, we can meet the needs of different customers with different purchase quantities.",
+    image: in12,
   },
   {
     title: "PROMPT DELIVERY",
     description:
       "Normally we can deliver the goods within 20 days after we confirmed the order.",
+    image: in13,
   },
 ];
 
@@ -26,14 +34,13 @@ const OurSpecialty = () => {
   return (
     <section className=" ">
       <div className=" ">
-        <h2 className="text-3xl font-bold ">Our Specialty</h2>
         <div className=" md:grid md:grid-cols-2 lg:grid-cols-4 justify-center">
           {specialties.map((specialty, index) => (
             <div
               key={index}
               className="bg-white group relative "
             >
-              <RotatingBox />
+              <RotatingBox specialty={specialty} />
               <div className="px-2 text-center bg-white max-h-0 overflow-hidden transition-all duration-1000 ease-in-out group-hover:max-h-96 absolute z-20 ">
                 <div className=" w-56 h-1 bg-red-600 mx-auto mt-16 mb-8 "> </div>
                 <p className="mb-4 text-lg text-gray-800">{specialty.description}</p>
--- src/components/RotatingBox/RotatingBox.js
+++ src/components/RotatingBox/RotatingBox.js
@@ -1,42 +1,38 @@
 "use client"
 
 import Image from 'next/image';
-import in10 from "@/images/icons/index_10.png";
-import in11 from "@/images/icons/index_11.png";
-import in12 from "@/images/icons/index_12.png";
-import in13 from "@/images/icons/index_13.png";
-
 import { useEffect, useRef } from 'react';
-const RotatingBox = () => {
+
+const RotatingBox = ({ specialty }) => {
     const rotatingBorderRef = useRef(null);
-  
+
     useEffect(() => {
-      const element = rotatingBorderRef.current;
-      let rotation = 0;
-  
-      const rotate = () => {
-        rotation += 0.3; // Decreased value to slow down the rotation
-        element.style.transform = `rotate(${rotation}deg)`;
-        requestAnimationFrame(rotate);
-      };
-  
-      rotate();
+        const element = rotatingBorderRef.current;
+        let rotation = 0;
+
+        const rotate = () => {
+            rotation += 0.3; // Decreased value to slow down the rotation
+            element.style.transform = `rotate(${rotation}deg)`;
+            requestAnimationFrame(rotate);
+        };
+
+        rotate();
     }, []);
-  
+
     return (
-      <div className="flex justify-center items-center  m-auto p-1 ">
-        <div className="relative w-60 h-60 flex items-center justify-center bg-white ">
-          <div
-            ref={rotatingBorderRef}
-            className="absolute inset-0 border-[4px] border-dashed border-gray-300 rounded-full"
-          ></div>
-          <div className=" flex flex-col items-center justify-center z-10 text-center">
-            <Image src={in10} alt="Icon" width={64} />
-            <p className="mt-4 text-xl font-semibold">Your Text Here</p>
-          </div>
+        <div className="flex justify-center items-center  m-auto p-1 ">
+            <div className="relative w-60 h-60 flex items-center justify-center bg-white ">
+                <div
+                    ref={rotatingBorderRef}
+                    className="absolute inset-0 border-[4px] border-dashed border-gray-300 rounded-full"
+                ></div>
+                <div className=" flex flex-col items-center justify-center z-10 text-center">
+                    <Image src={specialty.image} alt={specialty.title} width={64} />
+                    <p className="mt-4 text-lg ">{specialty.title}</p>
+                </div>
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default RotatingBox;
+};
+
+export default RotatingBox;
